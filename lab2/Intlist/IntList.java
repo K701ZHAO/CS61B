535754e3112a20574595d8d25d984a965b83d434
K701ZHAO <yangkai_zhao@163.com>
2021-07-17 14:56:57 +0800

    commit lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,22 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        /*
+        // iterative method
+        if(A.rest == null){
+            A.rest = B;
+        } else {
+            dcatenate(A.rest, B);
+        }
+        */
+
+        // recursive method
+        IntList L = A;
+        while(L.rest != null) {
+            L = L.rest;
+        }
+        L.rest = B;
+        return A;
     }
 
     /**
@@ -91,7 +106,12 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            return B;
+        }
+        return new IntList(A.first, catenate(A.rest, B));
+
+
     }
 
 
